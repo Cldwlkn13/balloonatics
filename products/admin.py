950cd4dbc57d1ee16e465d824ee92ce53df3404b
Cldwlkn13 <gavin_rudge@hotmail.co.uk>
2021-05-28 13:27:43 +0000

    Adjustments to admin view for products

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Product, Category, Material, Sub_Category
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'uuid',
+        'category',
+        'sub_category',
+        'material',
+        'name',
+        'price'
+    )
+
+
 admin.site.register(Category)
 admin.site.register(Material)
 admin.site.register(Sub_Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
