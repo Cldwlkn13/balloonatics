27257c211b01acc4984dac45e4dda8a3db29b118
Cldwlkn13 <gavin_rudge@hotmail.co.uk>
2021-05-31 14:44:33 +0000

    add to cart functionality added

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    
+    qty = int(request.POST.get('qty'))
+    this_url = request.POST.get('this_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += qty
+    else:
+        cart[item_id] = qty
+
+    request.session['cart'] = cart
+    return redirect(this_url)
