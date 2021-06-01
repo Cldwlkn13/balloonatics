d5d07fe438f7780b7fd82fcf69aa8a73d7e6c1b1
Cldwlkn13 <gavin_rudge@hotmail.co.uk>
2021-06-01 17:18:37 +0000

    finalised cart preview in toasts

--- cart/views.py
+++ cart/views.py
@@ -20,10 +20,11 @@ def add_to_cart(request, item_id):
         cart[item_id] += qty
     else:
         cart[item_id] = qty
-        messages.success(
-            request,
-            f'Added <strong>{product.name}</strong> to your cart!',
-            extra_tags='render_preview')
+
+    messages.success(
+        request,
+        f'Added {qty} x <strong>{product.name}</strong> to your cart!',
+        extra_tags='render_preview')
 
     request.session['cart'] = cart
     return redirect(this_url)
@@ -31,19 +32,29 @@ def add_to_cart(request, item_id):
 
 def update_cart(request, item_id):
 
-    this_url = request.POST.get('this_url')
+    product = get_object_or_404(Product, pk=item_id)
+    path = request.POST.get('this_url')
 
     if request.POST.get('qty') == '':
-        messages.error(request, 'Invalid quantity')
-        return redirect(this_url)
+        messages.error(request, 'Invalid quantity', '')
+        return redirect(path)
 
     qty = int(request.POST.get('qty'))
 
     cart = request.session.get('cart', {})
     cart[item_id] = qty
+    extra_tags = ''
+
+    if 'cart' not in path:
+        extra_tags = 'render_preview'
+
+    messages.info(
+        request,
+        f'Updated cart with {qty} x <strong>{product.name}</strong>!',
+        extra_tags=extra_tags)
 
     request.session['cart'] = cart
-    return redirect(this_url)
+    return redirect(path)
 
 
 def remove_from_cart(request, item_id):
@@ -51,14 +62,17 @@ def remove_from_cart(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     cart = request.session.get('cart', {})
     path = request.META['HTTP_REFERER']
-    print(path)
+    extra_tags = ''
+
+    if 'cart' not in path:
+        extra_tags = 'render_preview'
 
     if item_id in cart:
         cart.pop(item_id)
         messages.info(
             request,
             f'<strong>{product.name}</strong> removed from your cart!',
-            extra_tags='render_preview')
+            extra_tags=extra_tags)
 
     request.session['cart'] = cart
     return redirect(path)
