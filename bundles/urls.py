87edb76b84e79516fe722f4cf993a808029fe271
Gavin Rudge <gavin_rudge@hotmail.co.uk>
2021-06-13 13:37:05 +0100

    js functionality around rupdating price of bundle on item/ty changes

--- bundles/urls.py
+++ bundles/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('bundlecategories/', views.bundle_categories,
          name='bundle_categories'),
     path('withitems/<bundle_id>/', views.with_items, name='with_items'),
+    path('gettotalprice/', views.get_total_price, name='get_total_price'),
     path('serveimage/<int:product_id>/', views.serve_image,
          name='serve_image'),
 ]
--- bundles/views.py
+++ bundles/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import (render, HttpResponse,
 from .models import Bundle, BundleItem
 from .forms import BundleSelectorForm, BundleBuilderForm
 from products.models import Product
+from cart.helpers import custom_formset_dictionary_parser
 from django.forms import formset_factory
+
+from decimal import Decimal
 
 
 def bundle_categories(request):
@@ -76,3 +79,17 @@ def serve_image(request, product_id):
         return HttpResponse(content='../media/noimage.png', status=400)
 
     return HttpResponse(content=product.image.url, status=200)
+
+
+def get_total_price(request):
+    reqDict = request.body.decode("utf-8").split('&')
+    bundle_item_dict = custom_formset_dictionary_parser(reqDict)
+    total = 0
+    for item in bundle_item_dict.values():
+        if not item['product'] == '0':
+            product = Product.objects.get(
+                pk=item['product'])
+            item_total = Decimal(item['item_qty']) * product.discounted_price
+            total += item_total
+
+    return HttpResponse(content=total, status=200)
