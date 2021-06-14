3fed1e24a038021c3c7ecdeae71cbaf80311c7b0
Gavin Rudge <gavin_rudge@hotmail.co.uk>
2021-06-14 16:27:13 +0100

    set up db for deployment

--- balloonatics/settings.py
+++ balloonatics/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
+
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
-
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,13 +130,13 @@ WSGI_APPLICATION = 'balloonatics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
