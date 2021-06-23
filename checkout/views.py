801eb9a4f28820da223a9f7ea8a4fa9597c0564f
Gavin Rudge <gavin_rudge@hotmail.co.uk>
2021-06-23 21:05:13 +0100

    add some more comments to checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -45,13 +45,16 @@ def validate_order(request):
 
 
 def checkout(request):
+    # set the stripe keys
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
+    # load up the cart
     cart = request.session.get('cart', 
         {'products':{},'bundles':{},'custom_prints':{}})
 
     if request.method == 'POST':
+        # load up the address data from the request
         address_form_data = {
             'street_address_1': request.POST['street_address_1'],
             'street_address_2': request.POST['street_address_2'],
@@ -61,15 +64,18 @@ def checkout(request):
             'postal_code': request.POST['postal_code'],
         }
 
+        # load up the order form data from the request
         order_form_data = {
             'cust_name': request.POST['cust_name'],
             'cust_email': request.POST['cust_email'],
             'cust_phone': request.POST['cust_phone'],
         }
+        # init the forms with the data
         address_form = AddressForm(address_form_data)
         order_form = OrderForm(order_form_data)
 
         if address_form.is_valid() and order_form.is_valid():
+            # compile new address obj
             address = Address(
                 street_address_1=address_form_data['street_address_1'],
                 street_address_2=address_form_data['street_address_2'],
@@ -77,14 +83,18 @@ def checkout(request):
                 county_area=address_form_data['county_area'],
                 country=address_form_data['country'],
                 postal_code=address_form_data['postal_code'])
+            # give me a new uuid for the order pre save
             order_id = uuid.uuid4().hex.upper()
+            # compile new order obj
             order = Order(order_id=order_id,
                 cust_name=order_form_data['cust_name'],
                 cust_email=order_form_data['cust_email'],
                 cust_phone=order_form_data['cust_phone'],
                 address=address)
+            # stripe pids
             pid = request.POST.get('client_secret').split('_secret')[0]
             order.stripe_pid = pid
+            # dump the cart json
             order.original_cart = json.dumps(cart)
             order.save()
             
@@ -161,13 +171,15 @@ def checkout(request):
                 extra_tags='render_toast')
             return redirect(reverse('checkout'))
     else:
+        # load up the cart
         cart = request.session.get('cart',
             {'products':{},'bundles':{},'custom_prints':{}})
         if not cart:
             messages.error(request, "Your cart is empty",
-                           extra_tags='render_toast')
+                extra_tags='render_toast')
             return redirect(reverse('products'))
 
+        #load up the stripe payment intents from the cart total
         current_cart = cart_contents(request)
         total = current_cart['grand_total']
         stripe_total = round(total * 100)
@@ -219,6 +231,7 @@ def checkout_success(request, order_id):
     order_items_of_print = None
 
     try:
+        # get the order and its contents
         order = list(Order.objects.filter(order_id=order_id))[0]
         order_items_of_product = list(OrderItem.objects.filter(
             order__order_id=order_id, product__isnull=False))
@@ -227,6 +240,7 @@ def checkout_success(request, order_id):
         order_items_of_print = list(OrderItem.objects.filter(
             order__order_id=order_id, custom_print_order__isnull=False))
 
+        # load up the order bundles
         order_bundles = {}
         for order_item in order_items_of_bundle:
             bundle = Bundle.objects.get(bundle_id=order_item.bundle.bundle_id)
@@ -236,16 +250,18 @@ def checkout_success(request, order_id):
               
     except Exception:
         messages.error(request,
-                       'Could not confirm success of your Order '
-                       'Please contact us to confirm. ',
-                       extra_tags='render_toast')
+            'Could not confirm success of your Order '
+            'Please contact us to confirm. ',
+            extra_tags='render_toast')
         return redirect(reverse('checkout'))
 
     if request.user.is_authenticated:
+        # match up the user profile and attach
         profile = UserProfile.objects.get(user=request.user)
         order.user_profile = profile
         order.save()
 
+        # init profle obj 
         profile_data = {
             'cust_name': order.cust_name,
             'cust_email': order.cust_email,
@@ -258,12 +274,14 @@ def checkout_success(request, order_id):
             'postal_code': order.address.postal_code
         }
 
+        # if user happy to save then save the data from the order to the profile
         save_info = request.session.get('save_info')
         if save_info:
             user_profile_form = ProfileForm(profile_data, instance=profile)
             if user_profile_form.is_valid():
                 user_profile_form.save()
 
+        # get rid of the cart as we don't need it anymore
         if 'cart' in request.session:
             del request.session['cart']
 
