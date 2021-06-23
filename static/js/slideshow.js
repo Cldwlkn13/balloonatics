7935b81d4a6a6f7d51540e95af787ae2fc5cf780
Gavin Rudge <gavin_rudge@hotmail.co.uk>
2021-06-23 11:36:13 +0100

    small tidy on home page

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -1,31 +1,35 @@
 $(document).ready(function() {
     var currIndex = -1;
     var initial = true;
-    showSlides();
+    manageSlides();
 
-    function showSlides() {
+    function manageSlides() {
       $('.slideshow-container').each(function(e){
-        var slides = $(this).find(".slides");
-        var visibleSlides = $(this).find('.slides:visible');
-        if(visibleSlides.length > 0) {
-          slide = $(visibleSlides)[0];
-          currIndex = $(slide).attr('id').split('-')[2];
-        }
-        if(currIndex >= 0) {
-          var id = `#slide-${e}-${currIndex}`
-          $(id).fadeOut(500);
-        }
-        currIndex++;
-        if (currIndex > (slides.length - 1)) {
-          currIndex = 0;
-        }
-        var id = `#slide-${e}-0`
-        if(!initial){
-          id = `#slide-${e}-${currIndex}`
-        }
-        $(id).fadeIn(2000);
-      })
-      setTimeout(showSlides, 9500); 
+        showSlides($(this), e);
+      });
+      setTimeout(manageSlides, 10000); 
       initial = false;
+    }
+
+    function showSlides(container, e) {
+      var slides = $(container).find(".slides");
+      var visibleSlides = $(container).find('.slides:visible');
+      if(visibleSlides.length > 0) {
+        slide = $(visibleSlides)[0];
+        currIndex = $(slide).attr('id').split('-')[2];
+      }
+      if(currIndex >= 0) {
+        var id = `#slide-${e}-${currIndex}`
+        $(id).fadeOut(500);
+      }
+      currIndex++;
+      if (currIndex > (slides.length - 1)) {
+        currIndex = 0;
+      }
+      var id = `#slide-${e}-0`
+      if(!initial){
+        id = `#slide-${e}-${currIndex}`
+      }
+      $(id).fadeIn(2000);
     }
 })
