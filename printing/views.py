075e5e8b945b0b02a0e82210270aa2c3f218cba0
Gavin Rudge <gavin_rudge@hotmail.co.uk>
2021-06-23 20:32:24 +0100

    tidy up printing app

--- printing/views.py
+++ printing/views.py
@@ -19,6 +19,7 @@ def load_print_selector(request):
     
     products = Product.objects.filter(is_printable=True)
 
+    # load up slideshow images
     slideshow_images = {
         'age_2_pink.jpg': 'Our Wedding Bundle',
     }
@@ -33,16 +34,20 @@ def load_print_selector(request):
 
 def new_order_handler(request):
 
+    # get the printable balloons
     products = Product.objects.filter(is_printable=True)
 
     if request.method == 'POST':
 
+        # get the product from the request
         p_id = int(request.POST.get('p_id'))
         product = Product.objects.get(pk=p_id)
         
+        # get the custom message and qty from the request
         message = request.POST.get('custom_message')
         qty = request.POST.get('qty')
 
+        # load and save a new custom print order
         custom_print_order = CustomPrintOrder(
             base_product=product,
             custom_message=message, 
@@ -50,9 +55,11 @@ def new_order_handler(request):
         )
         custom_print_order.save()
 
+        # add it to the cart
         added_to_cart = add_or_update_custom_print_for_cart(
             request, custom_print_order.id).status_code == 200
 
+        # error message triggered in add_or_update_custom_print_for_cart if fail
         if not added_to_cart:
             return redirect(reverse('load_print_selector'))
 
@@ -64,6 +71,7 @@ def new_order_handler(request):
             to your cart!''',
             extra_tags='render_toast render_preview')
         
+        # load up the form with the data
         order_form = CustomPrintForm(message, qty)
         
         context = {
@@ -77,20 +85,21 @@ def new_order_handler(request):
     
     product = None
 
+    # if its not a product select request load up the selector form
     if not 'select_product' in request.GET:
         return redirect(reverse('load_print_selector'))
     
-    p_id = request.GET['select_product']
-    
+    # get id from request and try to get product 
+    p_id = request.GET['select_product']   
     try:
         product = Product.objects.get(pk=p_id)
     except Product.DoesNotExist:
         messages.warning(request,
             'Please select a valid product',
             extra_tags='render_toast')
-
         return redirect(reverse('load_print_selector'))
     
+    # load up an empty form
     order_form = CustomPrintForm('', 1)
 
     context = {
@@ -104,34 +113,43 @@ def new_order_handler(request):
 
 def update_order_handler(request, custom_print_id):
 
+    # get the referring url
     http_referrer = request.META['HTTP_REFERER']
 
+    # load up the printable products
     products = Product.objects.filter(is_printable=True)
     
     if request.method == 'POST':
+        # if product id not sent then load delector form
         if not 'p_id' in request.POST:
             return redirect(reverse('load_print_selector'))
         
+        # get the product from the request
         p_id = request.POST.get('p_id')
         product = Product.objects.get(pk=p_id)
 
+        # get the custom message and qty from the request
         message = request.POST.get('custom_message')
         qty = request.POST.get('qty')
 
+        # try load up the print order from the request id
         try:
             custom_print_order = CustomPrintOrder.objects.get(
                 pk=custom_print_id)
         except CustomPrintOrder.DoesNotExist:
             return redirect(reverse('load_print_selector'))
             
+        # update the print order
         custom_print_order.base_product=product
         custom_print_order.custom_message=message
         custom_print_order.qty=qty
         custom_print_order.save()
 
+        # update the order in the cart
         updated_in_cart = add_or_update_custom_print_for_cart(
             request, custom_print_id).status_code == 200
         
+        # error message triggered in add_or_update_custom_print_for_cart if fail
         if not updated_in_cart:
             return redirect(reverse(http_referrer))
 
@@ -143,6 +161,7 @@ def update_order_handler(request, custom_print_id):
             to your cart!''',
             extra_tags='render_toast render_preview')
         
+        # load up the form with the data
         order_form = CustomPrintForm(message, qty)
         
         context = {
@@ -154,12 +173,14 @@ def update_order_handler(request, custom_print_id):
         }
         return render(request, 'printing/printing.html', context)
 
+    # if get request then try get the print order
     try:
         custom_print_order = CustomPrintOrder.objects.get(
             pk=custom_print_id)
     except CustomPrintOrder.DoesNotExist:
         return redirect(reverse('load_print_selector'))
 
+    # load up the form with the data 
     order_form = CustomPrintForm(custom_print_order.custom_message, 
         custom_print_order.qty)
     
