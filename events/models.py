888d532d2614aa6ad525f6e2495bd2ad6b75b7dd
Gavin Rudge <gavin_rudge@hotmail.co.uk>
2021-06-26 16:37:57 +0100

    add phone number validator

--- events/models.py
+++ events/models.py
@@ -8,7 +8,7 @@ class Event(models.Model):
         validators=[alpha])
     your_email = models.EmailField(max_length=254, null=False, blank=False)
     your_number = models.CharField(max_length=20, null=False, blank=False, 
-        validators=[numeric])
+        validators=[phone])
     your_message = models.TextField(max_length=540, null=False, blank=False)
     event_type = models.ForeignKey('EventType', on_delete=models.SET_NULL,
                     null=True, blank=True)
--- validators.py
+++ validators.py
@@ -3,4 +3,5 @@ from django.core.validators import RegexValidator
 alpha = RegexValidator(r'^[a-zA-Z ]*$', 'Only letters are allowed.')
 alphanumeric = RegexValidator(r'^[0-9a-zA-Z ]*$', 'Only alphanumeric characters are allowed.')
 numeric = RegexValidator(r'^[0-9]*$', 'Only numbers are allowed.')
+phone = RegexValidator(r'^[0-9+]{10,13}$', 'Invalid Phone Number format.')
 
